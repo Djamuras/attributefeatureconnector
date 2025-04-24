5913ad5fc4ad7c55365c062447dd855731c92342
Djamuras <djamuras@gmail.com>
2025-04-24 15:37:21 +0300

    Update AdminAttributeFeatureConnectorController.php

--- controllers/admin/AdminAttributeFeatureConnectorController.php
+++ controllers/admin/AdminAttributeFeatureConnectorController.php
@@ -69,8 +69,25 @@ class AdminAttributeFeatureConnectorController extends ModuleAdminController
             }
         }
         
-        // Get existing mappings
-        $mappings = $this->getMappings();
+        // Pagination for mappings
+        $page = (int)Tools::getValue('page', 1);
+        $items_per_page = 10;
+        
+        // Get total count of mappings
+        $total_mappings = (int)Db::getInstance()->getValue('
+            SELECT COUNT(*) 
+            FROM ' . _DB_PREFIX_ . 'attribute_feature_mapping
+        ');
+        
+        $total_pages = ceil($total_mappings / $items_per_page);
+        if ($page < 1) {
+            $page = 1;
+        } elseif ($page > $total_pages && $total_pages > 0) {
+            $page = $total_pages;
+        }
+        
+        // Get existing mappings with pagination
+        $mappings = $this->getMappings($page, $items_per_page);
         
         // Get mapping being edited if applicable
         $mapping_to_edit = null;
@@ -87,7 +104,29 @@ class AdminAttributeFeatureConnectorController extends ModuleAdminController
                     break;
                 }
             }
+            
+            // If the mapping is not in the current page, fetch it separately
+            if (!$mapping_to_edit) {
+                $query = new DbQuery();
+                $query->select('afm.*, fvl.value, f.name as feature_name, GROUP_CONCAT(al.name SEPARATOR ", ") as attributes')
+                    ->from('attribute_feature_mapping', 'afm')
+                    ->leftJoin('attribute_feature_mapping_attributes', 'afma', 'afm.id_mapping = afma.id_mapping')
+                    ->leftJoin('feature_value_lang', 'fvl', 'afm.id_feature_value = fvl.id_feature_value AND fvl.id_lang = ' . (int)$this->context->language->id)
+                    ->leftJoin('feature_value', 'fv', 'fvl.id_feature_value = fv.id_feature_value')
+                    ->leftJoin('feature_lang', 'f', 'fv.id_feature = f.id_feature AND f.id_lang = ' . (int)$this->context->language->id)
+                    ->leftJoin('attribute_lang', 'al', 'afma.id_attribute = al.id_attribute AND al.id_lang = ' . (int)$this->context->language->id)
+                    ->where('afm.id_mapping = ' . (int)$edit_mapping_id)
+                    ->groupBy('afm.id_mapping');
+                
+                $result = Db::getInstance()->executeS($query);
+                if ($result && count($result) > 0) {
+                    $mapping_to_edit = $result[0];
+                    $selected_attributes = $this->getAttributesForMapping($edit_mapping_id);
+                }
+            }
         }
+        
+        $pagination_links = $this->generatePaginationLinks($page, $total_pages);
         
         $this->context->smarty->assign([
             'feature_options' => $feature_options,
@@ -95,13 +134,47 @@ class AdminAttributeFeatureConnectorController extends ModuleAdminController
             'mappings' => $mappings,
             'mapping_to_edit' => $mapping_to_edit,
             'selected_attributes' => $selected_attributes,
-            'generate_url' => $this->context->link->getAdminLink('AdminAttributeFeatureConnector') . '&action=generateFeatures',
+            'generate_url' => $this->context->link->getAdminLink('AdminAttributeFeatureConnector') . '&action=generateAllFeatures',
+            'generate_mapping_url' => $this->context->link->getAdminLink('AdminAttributeFeatureConnector') . '&action=generateFeatures&id_mapping=',
+            'undo_mapping_url' => $this->context->link->getAdminLink('AdminAttributeFeatureConnector') . '&action=undoMapping&id_mapping=',
             'delete_url' => $this->context->link->getAdminLink('AdminAttributeFeatureConnector') . '&action=deleteMapping',
             'edit_url' => $this->context->link->getAdminLink('AdminAttributeFeatureConnector') . '&action=editMapping',
             'cancel_url' => $this->context->link->getAdminLink('AdminAttributeFeatureConnector'),
+            'current_page' => $page,
+            'total_pages' => $total_pages,
+            'pagination_links' => $pagination_links,
+            'items_per_page' => $items_per_page,
+            'total_mappings' => $total_mappings
         ]);
         
         return $this->context->smarty->fetch(_PS_MODULE_DIR_ . 'attributefeatureconnector/views/templates/admin/configure.tpl');
+    }
+    
+    private function generatePaginationLinks($current_page, $total_pages)
+    {
+        $links = [];
+        $base_url = $this->context->link->getAdminLink('AdminAttributeFeatureConnector') . '&page=';
+        
+        // Previous link
+        if ($current_page > 1) {
+            $links['prev'] = $base_url . ($current_page - 1);
+        }
+        
+        // Page links
+        $links['pages'] = [];
+        $start_page = max(1, $current_page - 2);
+        $end_page = min($total_pages, $current_page + 2);
+        
+        for ($i = $start_page; $i <= $end_page; $i++) {
+            $links['pages'][$i] = $base_url . $i;
+        }
+        
+        // Next link
+        if ($current_page < $total_pages) {
+            $links['next'] = $base_url . ($current_page + 1);
+        }
+        
+        return $links;
     }
     
     public function postProcess()
@@ -128,12 +201,32 @@ class AdminAttributeFeatureConnectorController extends ModuleAdminController
             
             $this->updateMapping($id_mapping, $selected_attributes);
             $this->confirmations[] = $this->l('Mapping updated successfully');
-        } elseif (Tools::getValue('action') === 'generateFeatures') {
-            $result = $this->generateFeatures();
+        } elseif (Tools::getValue('action') === 'generateAllFeatures') {
+            $result = $this->generateAllFeatures();
             if ($result['success']) {
-                $this->confirmations[] = sprintf($this->l('Features generated successfully. %d products updated.'), $result['updated']);
+                $this->confirmations[] = sprintf($this->l('All features generated successfully. %d products updated.'), $result['updated']);
             } else {
                 $this->errors[] = $this->l('Error generating features');
+            }
+        } elseif (Tools::getValue('action') === 'generateFeatures') {
+            $id_mapping = (int)Tools::getValue('id_mapping');
+            if ($id_mapping) {
+                $result = $this->generateFeaturesForMapping($id_mapping);
+                if ($result['success']) {
+                    $this->confirmations[] = sprintf($this->l('Features for this mapping generated successfully. %d products updated.'), $result['updated']);
+                } else {
+                    $this->errors[] = $this->l('Error generating features for this mapping');
+                }
+            }
+        } elseif (Tools::getValue('action') === 'undoMapping') {
+            $id_mapping = (int)Tools::getValue('id_mapping');
+            if ($id_mapping) {
+                $result = $this->undoMapping($id_mapping);
+                if ($result['success']) {
+                    $this->confirmations[] = sprintf($this->l('Features for this mapping removed successfully. %d products updated.'), $result['updated']);
+                } else {
+                    $this->errors[] = $this->l('Error removing features for this mapping');
+                }
             }
         } elseif (Tools::getValue('action') === 'deleteMapping') {
             $id_mapping = (int)Tools::getValue('id_mapping');
@@ -146,9 +239,11 @@ class AdminAttributeFeatureConnectorController extends ModuleAdminController
         parent::postProcess();
     }
     
-    protected function getMappings()
+    protected function getMappings($page = 1, $items_per_page = 10)
     {
         $mappings = [];
+        $offset = ($page - 1) * $items_per_page;
+        
         $query = new DbQuery();
         $query->select('afm.*, fvl.value, f.name as feature_name, GROUP_CONCAT(al.name SEPARATOR ", ") as attributes')
             ->from('attribute_feature_mapping', 'afm')
@@ -157,7 +252,9 @@ class AdminAttributeFeatureConnectorController extends ModuleAdminController
             ->leftJoin('feature_value', 'fv', 'fvl.id_feature_value = fv.id_feature_value')
             ->leftJoin('feature_lang', 'f', 'fv.id_feature = f.id_feature AND f.id_lang = ' . (int)$this->context->language->id)
             ->leftJoin('attribute_lang', 'al', 'afma.id_attribute = al.id_attribute AND al.id_lang = ' . (int)$this->context->language->id)
-            ->groupBy('afm.id_mapping');
+            ->groupBy('afm.id_mapping')
+            ->orderBy('afm.date_add DESC')
+            ->limit($items_per_page, $offset);
         
         $result = Db::getInstance()->executeS($query);
         if ($result) {
@@ -237,7 +334,7 @@ class AdminAttributeFeatureConnectorController extends ModuleAdminController
         return true;
     }
     
-    protected function generateFeatures()
+    protected function generateAllFeatures()
     {
         $updated = 0;
         
@@ -264,47 +361,138 @@ class AdminAttributeFeatureConnectorController extends ModuleAdminController
         
         // Process each mapping
         foreach ($mappings as $id_feature_value => $attributes) {
-            // Get products with these attributes
-            $sql = 'SELECT DISTINCT pa.id_product 
-                    FROM ' . _DB_PREFIX_ . 'product_attribute_combination pac
-                    JOIN ' . _DB_PREFIX_ . 'product_attribute pa ON pa.id_product_attribute = pac.id_product_attribute
-                    WHERE pac.id_attribute IN (' . implode(',', array_map('intval', $attributes)) . ')';
-            
-            $products = Db::getInstance()->executeS($sql);
-            
-            if (!$products) {
-                continue;
-            }
-            
-            // Get feature information
-            $feature_value = new FeatureValue($id_feature_value);
-            $id_feature = $feature_value->id_feature;
-            
-            // Associate feature to products
-            foreach ($products as $product) {
-                $id_product = (int)$product['id_product'];
-                
-                // Check if the product already has this feature value
-                $exists = Db::getInstance()->getValue('
-                    SELECT COUNT(*)
-                    FROM ' . _DB_PREFIX_ . 'feature_product
-                    WHERE id_product = ' . $id_product . '
-                    AND id_feature = ' . $id_feature . '
-                    AND id_feature_value = ' . $id_feature_value
-                );
-                
-                if (!$exists) {
-                    // Add feature to product
-                    Db::getInstance()->insert('feature_product', [
-                        'id_feature' => $id_feature,
-                        'id_product' => $id_product,
-                        'id_feature_value' => $id_feature_value,
-                    ]);
-                    $updated++;
-                }
-            }
+            $updated += $this->processFeatureMapping($id_feature_value, $attributes);
         }
         
         return ['success' => true, 'updated' => $updated];
+    }
+    
+    protected function generateFeaturesForMapping($id_mapping)
+    {
+        $updated = 0;
+        
+        // Get mapping details
+        $query = new DbQuery();
+        $query->select('afm.id_feature_value, afma.id_attribute')
+            ->from('attribute_feature_mapping', 'afm')
+            ->leftJoin('attribute_feature_mapping_attributes', 'afma', 'afm.id_mapping = afma.id_mapping')
+            ->where('afm.id_mapping = ' . (int)$id_mapping);
+        
+        $result = Db::getInstance()->executeS($query);
+        
+        if (!$result) {
+            return ['success' => false, 'updated' => 0];
+        }
+        
+        // Organize the attribute IDs
+        $id_feature_value = $result[0]['id_feature_value'];
+        $attributes = [];
+        
+        foreach ($result as $row) {
+            $attributes[] = $row['id_attribute'];
+        }
+        
+        // Process the mapping
+        $updated = $this->processFeatureMapping($id_feature_value, $attributes);
+        
+        return ['success' => true, 'updated' => $updated];
+    }
+    
+    protected function undoMapping($id_mapping)
+    {
+        $updated = 0;
+        
+        // Get mapping details
+        $query = new DbQuery();
+        $query->select('afm.id_feature_value')
+            ->from('attribute_feature_mapping', 'afm')
+            ->where('afm.id_mapping = ' . (int)$id_mapping);
+        
+        $result = Db::getInstance()->getRow($query);
+        
+        if (!$result) {
+            return ['success' => false, 'updated' => 0];
+        }
+        
+        $id_feature_value = $result['id_feature_value'];
+        
+        // Get feature information
+        $feature_value = new FeatureValue($id_feature_value);
+        $id_feature = $feature_value->id_feature;
+        
+        // Get products with this mapping
+        $products_with_feature = Db::getInstance()->executeS('
+            SELECT id_product
+            FROM ' . _DB_PREFIX_ . 'feature_product
+            WHERE id_feature = ' . (int)$id_feature . '
+            AND id_feature_value = ' . (int)$id_feature_value
+        );
+        
+        if (!$products_with_feature) {
+            return ['success' => true, 'updated' => 0];
+        }
+        
+        // Remove features from products
+        foreach ($products_with_feature as $product) {
+            $id_product = (int)$product['id_product'];
+            
+            Db::getInstance()->delete(
+                'feature_product',
+                'id_feature = ' . (int)$id_feature . ' 
+                AND id_product = ' . (int)$id_product . ' 
+                AND id_feature_value = ' . (int)$id_feature_value
+            );
+            
+            $updated++;
+        }
+        
+        return ['success' => true, 'updated' => $updated];
+    }
+    
+    protected function processFeatureMapping($id_feature_value, $attributes)
+    {
+        $updated = 0;
+        
+        // Get products with these attributes
+        $sql = 'SELECT DISTINCT pa.id_product 
+                FROM ' . _DB_PREFIX_ . 'product_attribute_combination pac
+                JOIN ' . _DB_PREFIX_ . 'product_attribute pa ON pa.id_product_attribute = pac.id_product_attribute
+                WHERE pac.id_attribute IN (' . implode(',', array_map('intval', $attributes)) . ')';
+        
+        $products = Db::getInstance()->executeS($sql);
+        
+        if (!$products) {
+            return $updated;
+        }
+        
+        // Get feature information
+        $feature_value = new FeatureValue($id_feature_value);
+        $id_feature = $feature_value->id_feature;
+        
+        // Associate feature to products
+        foreach ($products as $product) {
+            $id_product = (int)$product['id_product'];
+            
+            // Check if the product already has this feature value
+            $exists = Db::getInstance()->getValue('
+                SELECT COUNT(*)
+                FROM ' . _DB_PREFIX_ . 'feature_product
+                WHERE id_product = ' . $id_product . '
+                AND id_feature = ' . $id_feature . '
+                AND id_feature_value = ' . $id_feature_value
+            );
+            
+            if (!$exists) {
+                // Add feature to product
+                Db::getInstance()->insert('feature_product', [
+                    'id_feature' => $id_feature,
+                    'id_product' => $id_product,
+                    'id_feature_value' => $id_feature_value,
+                ]);
+                $updated++;
+            }
+        }
+        
+        return $updated;
     }
 }
